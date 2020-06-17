fad0213b546a84c14de45f100d45992b545277bb
Galder Zamarreño <galder@zamarreno.com>
2020-06-17 11:34:23 +0200

    Add missing GraalVM packages present in JDK source cache

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/sources/SourceManager.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/sources/SourceManager.java
@@ -172,6 +172,15 @@ public class SourceManager {
                     "org.omg.",
                     "org.w3c.",
                     "org.xml",
+                    "org.graalvm.collections.",
+                    "org.graalvm.home.",
+                    "org.graalvm.nativeimage.",
+                    "org.graalvm.options.",
+                    "org.graalvm.polyglot.",
+                    "org.graalvm.word.",
+                    "com.oracle.truffe.api.",
+                    "com.oracle.truffe.object.",
+                    "com.oracle.truffe.polyglot.",
     };
     /**
      * A whitelist of packages prefixes used to pre-filter GraalVM class lookups.
